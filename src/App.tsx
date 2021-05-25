5d26f47848f30acc175447629fb2422b211de840
Marino <marinogomez24@gmail.com>
2021-05-25 17:12:56 -0400

    Added some tabs and placeholders

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,66 @@
-import React from 'react';
-import { Redirect, Route } from 'react-router-dom';
-import { IonApp, IonRouterOutlet } from '@ionic/react';
-import { IonReactRouter } from '@ionic/react-router';
-import Home from './pages/Home';
+import React from "react";
+import { Redirect, Route } from "react-router-dom";
+import {
+  IonApp,
+  IonRouterOutlet,
+  IonTabs,
+  IonTabBar,
+  IonTabButton,
+  IonIcon,
+  IonLabel,
+} from "@ionic/react";
+import { IonReactRouter } from "@ionic/react-router";
+
+import { calendar, person, map } from "ionicons/icons";
+
+/* Pages imports */
+import Home from "./pages/Home";
+import Dictionary from "./pages/Dictionary";
 
 /* Core CSS required for Ionic components to work properly */
-import '@ionic/react/css/core.css';
+import "@ionic/react/css/core.css";
 
 /* Basic CSS for apps built with Ionic */
-import '@ionic/react/css/normalize.css';
-import '@ionic/react/css/structure.css';
-import '@ionic/react/css/typography.css';
+import "@ionic/react/css/normalize.css";
+import "@ionic/react/css/structure.css";
+import "@ionic/react/css/typography.css";
 
 /* Optional CSS utils that can be commented out */
-import '@ionic/react/css/padding.css';
-import '@ionic/react/css/float-elements.css';
-import '@ionic/react/css/text-alignment.css';
-import '@ionic/react/css/text-transformation.css';
-import '@ionic/react/css/flex-utils.css';
-import '@ionic/react/css/display.css';
+import "@ionic/react/css/padding.css";
+import "@ionic/react/css/float-elements.css";
+import "@ionic/react/css/text-alignment.css";
+import "@ionic/react/css/text-transformation.css";
+import "@ionic/react/css/flex-utils.css";
+import "@ionic/react/css/display.css";
 
 /* Theme variables */
-import './theme/variables.css';
+import "./theme/variables.css";
 
 const App: React.FC = () => (
   <IonApp>
     <IonReactRouter>
-      <IonRouterOutlet>
-        <Route path="/home" component={Home} exact={true} />
-        <Route exact path="/" render={() => <Redirect to="/home" />} />
-      </IonRouterOutlet>
+      <IonTabs>
+        <IonRouterOutlet>
+          <Route path="/tab1" component={Home} exact={true} />
+          <Route path="/tab2" component={Dictionary} exact={true} />
+          <Route path="/tab3" component={Home} />
+          <Route path="/" render={() => <Redirect to="/tab1" />} exact={true} />
+        </IonRouterOutlet>
+        <IonTabBar slot="bottom">
+          <IonTabButton tab="tab1" href="/tab1">
+            <IonIcon icon={calendar} />
+            <IonLabel>Tab 1</IonLabel>
+          </IonTabButton>
+          <IonTabButton tab="tab2" href="/tab2">
+            <IonIcon icon={person} />
+            <IonLabel>Tab 2</IonLabel>
+          </IonTabButton>
+          <IonTabButton tab="tab3" href="/tab3">
+            <IonIcon icon={map} />
+            <IonLabel>Tab 3</IonLabel>
+          </IonTabButton>
+        </IonTabBar>
+      </IonTabs>
     </IonReactRouter>
   </IonApp>
 );
---
+++ src/pages/Dictionary.tsx
@@ -0,0 +1,66 @@
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonSearchbar,
+  IonList,
+  IonItem,
+  IonLabel,
+  IonButton,
+} from "@ionic/react";
+import React, { useState } from "react";
+
+import translate from "google-translate-open-api";
+
+const searchFunction = async (word: string) => {
+  const result = await translate(word, {
+    tld: "cn",
+    to: "zh-CN",
+  });
+  const data = result.data[0];
+  return data;
+};
+
+const Dictionary: React.FC = () => {
+  const [searchText, setSearchText] = useState("");
+  let searchResults: string[] = [];
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Dictionary</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent className="ion-padding">
+        <p>Searchbar with cancel button shown on focus</p>
+        <IonSearchbar
+          value={searchText}
+          onIonChange={(e) => setSearchText(e.detail.value!)}
+          showCancelButton="focus"
+        ></IonSearchbar>
+        <IonButton
+          color="primary"
+          onClick={(event: React.MouseEvent<HTMLElement>) => {
+            searchFunction(searchText);
+          }}
+        >
+          Search
+        </IonButton>
+        <IonList>
+          {searchResults.length > 0 ? (
+            <>
+              {console.log(searchResults)}
+              <p>Found something</p>
+            </>
+          ) : (
+            <p>Not found anything</p>
+          )}
+        </IonList>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Dictionary;
